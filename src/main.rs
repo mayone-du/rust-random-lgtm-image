c522fcf57beca15b7535a97ddc474c9a9d8cec2f
mayone-du <cocomayo1201@icloud.com>
2021-10-23 22:39:39 +0900

    Add reqwest url

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use rand::{thread_rng, Rng};
+use reqwest;
+
+#[tokio::main]
+async fn main() {
+    let mut rng = thread_rng();
+    let rand_num: u32 = rng.gen_range(130000..150000);
+    let req_url = format!("https://image.lgtmoon.dev/{}", rand_num);
+    println!("request url is {}", req_url);
+    let res = reqwest::get(req_url).await;
+    println!("{:?}", res);
 }
