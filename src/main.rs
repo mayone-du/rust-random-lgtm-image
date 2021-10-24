4b5116822b82d7aca7aacd0a2784aeb85f6abb2c
mayone-du <cocomayo1201@icloud.com>
2021-10-24 22:20:30 +0900

    Add memo comment

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,17 @@ use reqwest;
 use std::fs::File;
 use std::io::Write;
 
+// TODO: 普通の画像をランダムにダウンロードし、中央にLGTMの文字を出力して画像に書き出す
+
+// LGTM画像をランダムにダウンロードする
 #[tokio::main]
 async fn main() {
     let mut rng = thread_rng();
+    // ランダムな整数を生成
     let rand_num: u32 = rng.gen_range(130000..150000);
+    // リクエストするURLを定義
     let req_url = format!("https://image.lgtmoon.dev/{}", rand_num);
-    println!("request url is {}", req_url);
+    println!("Request url is {}", req_url);
     let res = match reqwest::get(req_url).await {
         Ok(response) => {
             println!("OK. Response is : {:?}", response);
@@ -19,13 +24,20 @@ async fn main() {
         }
     };
 
-    let image_bytes = res.bytes().await.expect("Error! to bytes");
-    let mut buffer = File::create("hoge.jpeg").expect("Error! file create");
+    // レスポンスのステータスコードを取得
+    let status_code = res.status();
 
-    let mut pos = 0;
+    if status_code == 200 {
+        let image_bytes = res.bytes().await.expect("Error! to bytes");
+        let mut buffer = File::create("lgtm.jpg").expect("Error! file create");
 
-    while pos < image_bytes.len() {
-        let bytes_written = buffer.write(&image_bytes[pos..]).expect("Error!");
-        pos += bytes_written;
+        // TODO: 解読
+        let mut pos = 0;
+        while pos < image_bytes.len() {
+            let bytes_written = buffer.write(&image_bytes[pos..]).expect("Error!");
+            pos += bytes_written;
+        }
+    } else if status_code == 403 {
+        panic!("Request Error!");
     }
 }
